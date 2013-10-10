ceb5840ab8dd2c1fd8029b6b7473fd35a7fac52d
Byron Hinebaugh <bhinebaugh@gmail.com>
2013-10-10 13:02:17 -0400

    adjustments to flexslider navigation styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,11 @@ $(function(){
 	$('.posts').flexslider({
 		direction : 'vertical',
 		pauseOnHover : true,
+                directionNav : true,
 		controlNav : true,
+                controlsContainer : '.post-nav'
+                //~ start : function(slider) {
+                                //~ $('.post-nav').on('click', function(){ slider.flexAnimate(3) });
+                        //~ }
 	});
 });

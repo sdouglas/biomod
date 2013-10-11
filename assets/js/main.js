cb99465151cba7b47f62fc13ebbea0983ca004a3
Byron Hinebaugh <bhinebaugh@gmail.com>
2013-10-11 19:39:13 -0400

    countdown timer

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,19 @@ $(function(){
                                 //~ $('.post-nav').on('click', function(){ slider.flexAnimate(3) });
                         //~ }
 	});
+        
+        
+        var nextBiomod = new Date( 2013, 11-1, 2, 10);   // month is 0-indexed, so subtract 1
+        $('.countdown').countdown({ 
+                // Since jamboree is in Cambridge, we make sure time is EST, 5 hours behind UTC
+                until: $.countdown.UTCDate(-5, nextBiomod), 
+                format : 'dHM',
+                // {h<} means omit hour from display when it reaches zero
+                layout :'{d<}<div class="days">{dn}</div><div>{dl}</div>{d>}' +
+                        '<div class="time-block">' +
+                        '{h<}<div class="hours">{hn}</div><div>{hl}</div>{h>}' +
+                        '</div><div class="time-block">' +
+                        '<div class="minutes">{mn}</div><div>{ml}</div>' +
+                        '</div>'
+        });
 });
